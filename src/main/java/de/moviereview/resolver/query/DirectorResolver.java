123c6ff9adafc6f0f86a5ae7dec7738ad81f2f0e
angie-beep <c00kiemittwoch@gmail.com>
2024-12-02 17:08:36 +0100

    edit director classes

--- src/main/java/de/moviereview/resolver/query/DirectorResolver.java
+++ src/main/java/de/moviereview/resolver/query/DirectorResolver.java
@@ -1,4 +1,26 @@
 package de.moviereview.resolver.query;
 
-public class DirectorResolver {
+import de.moviereview.domain.model.Director;
+import de.moviereview.domain.service.DirectorService;
+import graphql.kickstart.tools.GraphQLQueryResolver;
+import java.util.List;
+
+
+public class DirectorResolver implements GraphQLQueryResolver {
+    private final DirectorService directorService;
+
+    // Constructor-based dependency injection
+    public DirectorResolver(DirectorService directorService) {
+        this.directorService = directorService;
+    }
+
+    // Query: Fetch all actors
+    public List<Director> getDirector() {
+        return directorService.getAllDirector();
+    }
+
+    // Query: Fetch actor by ID
+    public Director getDirectorById(Long id) {
+        return directorService.getDirectorById(id);
+    }
 }
